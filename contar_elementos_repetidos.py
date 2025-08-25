a498a4320e9e82235149e24fb7e8d90717fc7870
Emmanuel <emmanuelpenuela@gmail.com>
2025-08-25 11:48:19 -0500

    Eliminar elementos repetidos (versión 2)

--- contar_elementos_repetidos.py
+++ contar_elementos_repetidos.py
@@ -1,9 +1,12 @@
-contador = 0
-lista_repetidos = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
-def contar_elementos_repetidos(lista):
-    global contador
-    for elemento in lista:
-        if elemento in lista_repetidos:
-            contador += 1
-    return contador
-print(contar_elementos_repetidos(lista_repetidos))
+lista_repetidos = [2, 3, 5, 7, 11, 13, 17, 2, 2, 29, 31, 37, 2, 43, 47]
+def sin_duplicados(lista):
+    vistos = set()
+    resultado = []
+    for x in lista:
+        if x not in vistos:
+            vistos.add(x)
+            resultado.append(x)
+    return resultado
+
+lista = [3, 1, 2, 3, 2, 4, 1]
+print(sin_duplicados(lista_repetidos))
